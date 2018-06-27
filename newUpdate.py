1e698b03a6d05cfd8d97ef1c06fdd911665e504e
divjot0 <33392716+divjot0@users.noreply.github.com>
2018-06-27 16:03:07 +0530

    Update newUpdate.py

--- newUpdate.py
+++ newUpdate.py
@@ -9,6 +9,11 @@ def getAddress(rawTransaction, outputIndex):
 	else:
 		return None
 
+""" Checks for same sets
+	Combining and deleting sets
+	Test for largest"""
+		
+
 
 myDict = requests.get('http://localhost:8332/rest/block/0000000000000832e0f70b86d818755ab0aef51541e1f2bbe5146784daaa517c.json').json()
 jStr = json.dumps(myDict)
@@ -16,6 +21,7 @@ block = json.loads(jStr)
 
 listOfSets = []
 
+i=1
 for transaction in block['tx']:
 	A = set([])
 	for inputTransaction in transaction['vin']:
@@ -23,18 +29,23 @@ for transaction in block['tx']:
 			result = subprocess.run(["bitcoin-cli", "getrawtransaction", str(inputTransaction['txid']), "1"], stdout=subprocess.PIPE)
 			a = result.stdout.decode()
 			r = getAddress(a, inputTransaction['vout'])
-			print(r)
 			if r!=None:
-				A.add(r)
-				print(r)
-		else:
-			print("Coinbase")
+				if r not in A:
+					A.add(r)
 	listOfSets.append(A)
-	print ("\nnext transaction")
+	print(i)
+	i=i+1
+print("Done")
 
 
 for u in listOfSets:
-	print (u)
-	print ("\n\n\n\n")
+	for address in u:
+		for v in listOfSets:
+			if address in v:
+				u = u.union(v)
+				listOfSets.remove(v)
+
+for u in listOfSets:
+	print(u)
 
 """REMOVE YOUR ID AND PASSWORD"""

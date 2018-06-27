f1e48fa5c89dab6575ad9e25c04bb4bdb14f0777
divjot0 <33392716+divjot0@users.noreply.github.com>
2018-06-27 18:49:35 +0530

    Update TransactionGraph.py

--- TransactionGraph.py
+++ TransactionGraph.py
@@ -1,7 +1,43 @@
+import json
+import requests
 import subprocess
 
-start_block = int(input())
-end_block = int(input())
+def getAddress(rawTransaction, outputIndex):
+	tx = json.loads(rawTransaction)
+	if tx['vout'][outputIndex]['scriptPubKey']['type']=="pubkeyhash":
+		return tx['vout'][outputIndex]['scriptPubKey']['addresses'][0]
+	else:
+		return None
+
+def getBlockSets(blockHash):
+	myDict = requests.get('http://localhost:8332/rest/block/0000000000000832e0f70b86d818755ab0aef51541e1f2bbe5146784daaa517c.json').json()
+	jStr = json.dumps(myDict)
+	block = json.loads(jStr)
+
+	listOfSetsInBlock = []
+
+	i=1
+	for transaction in block['tx']:
+		A = set([])
+		for inputTransaction in transaction['vin']:
+			if 'txid' in inputTransaction:
+				result = subprocess.run(["bitcoin-cli", "getrawtransaction", str(inputTransaction['txid']), "1"], stdout=subprocess.PIPE)
+				a = result.stdout.decode()
+				r = getAddress(a, inputTransaction['vout'])
+				if r!=None:
+					if r not in A:
+						A.add(r)
+		listOfSetsInBlock.append(A)
+		print(i)
+		i=i+1
+	print("Done")
+	return listOfSetsInBlock
+		
+
+listOfSets = []
+
+start_block = int(input("Enter start block"))
+end_block = int(input("Enter end block"))
 block_hashes = []
 command = "bitcoin-cli"
 
@@ -9,5 +45,21 @@ for x in range(start_block, end_block):
 	result = subprocess.run([command, 'getblockhash', str(x)], stdout=subprocess.PIPE)
 	block_hashes.append(result.stdout.decode()[:-1])
 
-for block_hash in block_hashes:
-	print (block_hash)
+print("Got all block hashes. Now starting to get address")
+
+for blkhash in block_hashes:
+	listOfSets.append(getBlockSets(blkhash))
+
+print("Got the sets of individual transactions. Now widening the entities.")
+
+for u in listOfSets:
+	for address in u:
+		for v in listOfSets:
+			if address in v:
+				u = u.union(v)
+				listOfSets.remove(v)
+
+print("Done with all the processing. Now starting to print the results.")
+
+for u in listOfSets:
+	print(u)
